5d87f7955347ab0f2e998646f7ae224cff7c19ea
Arjan Zijderveld <arjan@web3scan.eu>
2019-11-28 19:36:14 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     #   py_modules=["my_module"],
     #
     #packages=find_packages(exclude=['contrib', 'docs', 'tests', 'test']),  # Required
-    packages=['substrateinterface'],  # Required
+    packages=find_packages(),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
