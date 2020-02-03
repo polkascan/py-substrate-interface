7480416c53f3ea72c91c7dbf17b6b0bfda5bd7ba
Arjan Zijderveld <arjan@web3scan.eu>
2020-02-03 14:54:30 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1',  # Required
+    version='0.9.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -153,8 +153,8 @@ setup(
         'idna==2.8',
         'requests==2.22.0',
         'urllib3==1.25.3',
-        'xxhash==1.4.3',
-        'scalecodec>=0.9.1'
+        'xxhash==1.3.0',
+        'scalecodec>=0.9.2'
     ],
 
     # List additional groups of dependencies here (e.g. development
