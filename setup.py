d98fd0ab1b735a4d9399cc0f1c63b65750ec7d78
Arjan Zijderveld <arjan@web3scan.eu>
2020-03-07 12:18:33 +0100

    Scalecodec version bump

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ setup(
         'requests==2.22.0',
         'urllib3==1.25.3',
         'xxhash==1.3.0',
-        'scalecodec>=0.9.3'
+        'scalecodec>=0.9.19'
     ],
 
     # List additional groups of dependencies here (e.g. development
