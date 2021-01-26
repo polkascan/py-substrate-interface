03e0ed0038ff014161f90d4c5041344b28e393c5
Arjan Zijderveld <arjan@web3scan.eu>
2021-01-26 17:58:16 +0100

    Python 3.9 support

--- setup.py
+++ setup.py
@@ -139,6 +139,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # This field adds keywords for your project which will appear on the
