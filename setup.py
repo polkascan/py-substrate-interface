9b0cfca4e6ef9f19a913bf4c1c935de099ce9fb4
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2021-05-03 17:46:39 +0200

    Scalecodec version bump

--- setup.py
+++ setup.py
@@ -182,7 +182,7 @@ setup(
         'idna>=2.8',
         'requests~=2.25.1',
         'xxhash>=1.3.0',
-        'scalecodec~=0.10.84',
+        'scalecodec~=0.10.85',
         'py-sr25519-bindings~=0.1.2',
         'py-ed25519-bindings~=0.1.2',
         'py-bip39-bindings~=0.1.6'
