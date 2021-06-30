09219f97cc6827f967ec1114f4fa896b6f786861
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2021-06-30 15:49:26 +0200

    Updated Docker image version

--- examples/start_local_substrate_node.sh
+++ examples/start_local_substrate_node.sh
@@ -1,1 +1,1 @@
-docker run -p 9933:9933 -p 9944:9944 -p 9615:9615 -v substrate-dev:/substrate parity/substrate:2.0.0-533bbbd --dev --tmp --unsafe-ws-external --rpc-cors=all --unsafe-rpc-external --rpc-methods=Unsafe --prometheus-external
+docker run -p 9933:9933 -p 9944:9944 -p 9615:9615 -v substrate-dev:/substrate parity/substrate:2.0.0-631d4cdbca --dev --tmp --unsafe-ws-external --rpc-cors=all --unsafe-rpc-external --rpc-methods=Unsafe --prometheus-external
