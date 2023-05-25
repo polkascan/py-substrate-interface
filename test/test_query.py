157b1e657703c51786180350d22cec8df299da43
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2023-05-25 13:22:32 +0200

    Updated unit test to reflect new type default

--- test/test_query.py
+++ test/test_query.py
@@ -62,7 +62,7 @@ class QueryTestCase(unittest.TestCase):
             {
                 'nonce': 0, 'consumers': 0, 'providers': 0, 'sufficients': 0,
                 'data': {
-                    'free': 0, 'reserved': 0, 'misc_frozen': 0, 'fee_frozen': 0
+                    'free': 0, 'reserved': 0, 'frozen': 0, 'flags': 170141183460469231731687303715884105728
                 }
             }, result.value)
 
